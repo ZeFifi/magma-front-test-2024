06eb5849ab89d3efa9d968c9ca4ec21fdc2f506c
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2024-04-12 09:58:10 +0200

    Create the ambassadors store

---
+++ src/stores/ambassadors.ts
@@ -0,0 +1,42 @@
+// NPM
+import { defineStore } from "pinia";
+
+export interface Ambassador {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+}
+
+export const useAmbassadorsStore = defineStore("ambassadors", {
+  state: () => ({
+    organizationId: 0,
+    ambassadorFirstname: "",
+    ambassadorLastname: "",
+    ambassadorEmail: "",
+    allAmbassadors: [] as {
+      id: number;
+      firstname: string;
+      lastname: string;
+      email: string;
+    }[]
+  }),
+
+  actions: {
+    setOrganizationId(id: number) {
+      this.organizationId = id;
+    },
+
+    setAmbassadorFirstname(firstname: string) {
+      this.ambassadorFirstname = firstname;
+    },
+
+    setAmbassadorLastname(lastname: string) {
+      this.ambassadorLastname = lastname;
+    },
+
+    setAmbassadorEmail(email: string) {
+      this.ambassadorEmail = email;
+    }
+  }
+});
