dd378e000ee870479bc82b490166183fa2c42796
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2024-04-12 10:06:27 +0200

    Display component views depending on the current route

--- src/views/signup/SignupLayout.vue
+++ src/views/signup/SignupLayout.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 // Components
+import FooterNext from "@/components/FooterNext.vue";
 import SignupSidebar from "@/components/shell/SignupSidebar.vue";
+import { useRoute } from "vue-router";
+import SignupAmbassadorsView from "./SignupAmbassadorsView.vue";
+import SignupOrganizationView from "./SignupOrganizationView.vue";
+
+const route = useRoute();
 </script>
 
 <template>
   <div :class="[{ flex: true }]">
     <SignupSidebar />
+    <template v-if="route.path === '/sign-up/organization'">
+      <SignupOrganizationView
+    /></template>
+    <template v-else><SignupAmbassadorsView /></template>
   </div>
+  <FooterNext />
 </template>
