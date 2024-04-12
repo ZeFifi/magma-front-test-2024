3dab4f7abffe5dba186f883fc48a2819f6fe3c84
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2024-04-12 10:03:14 +0200

    Create the WarningMessage component

---
+++ src/components/WarningMessage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// Imports
+import { defineProps } from "vue";
+
+// Props
+const props = defineProps({
+  message: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="rounded-lg p-4 bg-background-warning-default mb-6">
+    <p class="text-sm">
+      {{ props.message }}
+    </p>
+  </div>
+</template>
