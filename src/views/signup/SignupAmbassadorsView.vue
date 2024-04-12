51bf19a49269653647f177b3bfd3c435df1e5540
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2024-04-12 10:07:13 +0200

    Create the SignupAmbassadorsView

--- src/views/signup/SignupAmbassadorsView.vue
+++ src/views/signup/SignupAmbassadorsView.vue
@@ -1,12 +1,55 @@
 <script setup lang="ts">
-// Components
-import {
-  BaseButton,
-  BaseIcon,
-} from "magma-design-system-test";
+// Imports
+import { useAmbassadorsStore } from "@/stores/ambassadors";
+import { ref } from "vue";
 
+import AddAmbassadorsBlock from "@/components/AddAmbassadorsBlock.vue";
+import AmbassadorModal from "@/components/AmbassadorModal.vue";
+import AmbassadorsList from "@/components/AmbassadorsList.vue";
+import WarningMessage from "@/components/WarningMessage.vue";
+
+// Variables
+const ambassadorStore = useAmbassadorsStore();
+const ambassadors = ambassadorStore.allAmbassadors;
+
+// Functions
+
+// Checks if there are ambassadors
+const hasAmbassadors = () => {
+  if (ambassadors.length > 0) {
+    return true;
+  }
+  return false;
+};
+
+// Toggles the modal
+const toggleModal = () => {
+  isModalOpen.value = !isModalOpen.value;
+};
+
+// States
+let isModalOpen = ref(false);
 </script>
 
 <template>
-  <div></div>
+  <div class="mt-[152px] w-[728px]">
+    <div class="ml-32">
+      <h1 class="text-mgm-txt-xl mb-6">
+        {{ $t("signupAmbassadorsView.heading") }}
+      </h1>
+      <div>
+        <WarningMessage :message="$t('signupAmbassadorsView.advice')" />
+        <AddAmbassadorsBlock
+          class="mb-10"
+          :message="$t('signupAmbassadorsView.description')"
+          :toggleModal="toggleModal"
+          :ambassadors="ambassadors"
+        />
+        <AmbassadorsList v-if="hasAmbassadors()" :ambassadors="ambassadors" />
+      </div>
+    </div>
+    <div v-if="isModalOpen">
+      <AmbassadorModal @close="isModalOpen = false" />
+    </div>
+  </div>
 </template>
